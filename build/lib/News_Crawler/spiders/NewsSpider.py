a6bf594e9e230c51a6e46ba23d5fe9c85fdb0f59
quancq <quancq.it@gmail.com>
2018-09-02 00:54:53 +0700

    Update and first try to deploy Scrapinghub

---
+++ build/lib/News_Crawler/spiders/NewsSpider.py
@@ -0,0 +1,23 @@
+import scrapy
+from News_Crawler import utils
+
+
+class NewsSpider(scrapy.Spider):
+    def __init__(self, name=None, **kwargs):
+        super(NewsSpider, self).__init__(name=name, **kwargs)
+        self.page_per_category_limit = utils.get_crawl_limit(name)
+        self.article_scraped_count = 0
+        self.lang = "vi"
+
+    def parse(self, response):
+        raise NotImplementedError()
+
+    def parse_category(self, response):
+        raise NotImplementedError()
+
+    def parse_article(self, response):
+        raise NotImplementedError()
+
+    def transform_time_fmt(self, time_str, src_fmt):
+        return utils.transform_time_fmt(time_str, src_fmt=src_fmt)
+
